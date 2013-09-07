56af3228f33fd71f917c556b72da53030d296b6d
Dain Miller <miller.dain@gmail.com>
2013-09-07 01:17:22 -0400

    Added comments, and a bit of naming updates.
    - Updated the name of the typeofBackbone variable.
    - Added some comments for various areas, that are both ideas and things
      for me to keep in mind as I move forward.

--- assets/js/backbone/mixins/autocomplete.js
+++ assets/js/backbone/mixins/autocomplete.js
@@ -1,4 +1,9 @@
-(function ($) {
+// The semicolon at the beginning is in case of another library breaking ours (prevents ASI errors).
+// This plugin is being created with using absolutely 0 semi-colons.  We are using our knowledge of Automatic
+// semicolon insertion (ASI) in JavaScript to do that for us, so we don't have to.  It's an experiment in cleanliness
+// if nothing else.
+
+;(function ($) {
 
   $.fn.midasAutocomplete = function (options) {
     options = options || {}
@@ -8,9 +13,13 @@
         type                  = options.type,
         $inputData            = $input.val(),
         backbone              = options.backbone,
+        contentType           = options.contentType,
         apiEndpoint           = options.apiEndpoint,
         backboneEvents        = options.backboneEvents,
-        searchResultsWrapper  = options.searchResultsClass
+        searchResultsWrapper  = options.searchResultsClass,
+
+        // Here they can override the success and failure messages completely
+        // But this override is not implemented yet.
         success               = options.success || (function () {}),
         failure               = options.failure || (function () {})
 
@@ -27,9 +36,9 @@
     }
 
     function fetchDataUsingBackboneOrAjax () {
-      var typeOfBackbone = typeof backbone;
+      var typeOfBackboneParam = typeof backbone
 
-      switch (typeOfBackbone) {
+      switch (typeOfBackboneParam) {
         case "object":
           initializeBackboneAutocomplete()
           break
@@ -57,7 +66,7 @@
     }
 
     function initializeBackboneAutocomplete () {
-      console.log("Not yet implemented");
+      console.log("Not yet implemented")
     }
 
 
@@ -71,17 +80,25 @@
 
         url: apiEndpoint + $inputData,
         type: type,
+        contentType: contentType,
 
         success: function (data) {
+
+          // This won't work, but somehow check if they have set it on the front-end and 
+          // if so use that success method instead of this one.
+          // if (success) {
+            // success(data) // here what I am trying to show is we want to pass data to success method
+          // },
+
           var i = 0,
-              results = data.length;
+              results = data.length
 
           // On each new successful character search, 
           // replace the previous results, and append the new results.
-          $(searchResultsWrapper).children().remove();
+          $(searchResultsWrapper).children().remove()
 
           for ( ; i < results; i += 1 ) {
-            $(searchResultsWrapper).append(template(data[i]));  
+            $(searchResultsWrapper).append(template(data[i]))
           }
 
         },
@@ -90,13 +107,24 @@
           if (!err.responseText) console.warn("Please add some response text to your server side error")
           if (typeof err.responseText === "string") this.json = JSON.parse(err.responseText)
 
-          alert("Expects a hash of { 'message': 'error message' } (Here's ours):" + " " + this.json.message);
+          alert("Expects a hash of { 'message': 'error message' } (Here's ours):" + " " + this.json.message)
         }
 
-      });
+      })
     }
 
+    // The template classes don't really need to be customized, they can just expect them and then
+    // style to that effect.  The reason they don't need to be customized is because they are being appended
+    // to a customizable container.  So with that it's enough and we can just allow them to expect this.
     function template (result) {
+      // Here we are going to want to abstract out what the result.____ methods are
+      // so for instance if the result.target is something that is used, then we use that.
+      // but if instead the user wants to send something back from the server like { foo: 'val', bar: 'val' }
+      // then we somehow need to be able to handle it here in the template.
+      // 
+      // OR the other optinos is to say that these words target and value are standardized enough for an autocomplete
+      // that they can then craft the backend to match this API expectation on the front-end.  I think that is a more
+      // elegant solution.  
       return " " +
         "<div class='user-photo'> " + 
           "<div class='search-result-row'> " +
